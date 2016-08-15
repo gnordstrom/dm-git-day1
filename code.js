8f3b9f54d891a02b0055f84feaadce943e6362c1
Gustav Nordstrom <gnordstrom@users.noreply.github.com>
2016-08-15 14:38:35 -0600

    added command line and git commands

--- code.js
+++ code.js
@@ -1,1 +1,18 @@
 var journal = ["DM13 rocks!!! WOOOO!!", "I'm excited about this!", "Utah has mountains."];
+
+/*
+cd foldername
+cd ..
+cd ~
+ls
+mkdir newfoldername
+touch newfilename
+
+
+git clone
+git status
+git add filename.js
+git commit -m "message"
+git push
+
+*/
